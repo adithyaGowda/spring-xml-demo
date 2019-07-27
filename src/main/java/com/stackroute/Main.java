23a81745f59e71a6e3679d31df384380adfd75bc
adithyaGowda <adithyagowda5696@gmail.com>
2019-07-27 19:02:25 +0530

    adding modified files

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Actor actorRef1 = context.getBean("actor", Actor.class);
+        Actor actorRef1 = context.getBean("actor1", Actor.class);
         Actor actorRef2 = context.getBean("actor2", Actor.class);
         Actor actorRef3 = context.getBean("actor3", Actor.class);
 
@@ -24,10 +24,10 @@ public class Main {
 
 
         Movie movieRef1 = context.getBean("movie1",Movie.class);
-
         System.out.println(movieRef1.toString());
 
-       
+        Movie movieRef2 = context.getBean("movie2",Movie.class);
+        System.out.println(movieRef2.toString());
 
 
 
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -4,6 +4,15 @@ public class Movie {
 
     Actor actor1, actor2, actor3;
 
+    public Movie() {
+    }
+
+    public Movie(Actor actor1, Actor actor2, Actor actor3) {
+        this.actor1 = actor1;
+        this.actor2 = actor2;
+        this.actor3 = actor3;
+    }
+
     public void setActor1(Actor actor1) {
         this.actor1 = actor1;
     }
