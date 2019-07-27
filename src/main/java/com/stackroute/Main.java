962c07644f702eca6af5d83689126e16b75bab48
adithyaGowda <adithyagowda5696@gmail.com>
2019-07-27 15:19:28 +0530

    adding files to autowire-xml directory

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -13,19 +13,25 @@ public class Main {
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Actor actorRef = context.getBean("actor", Actor.class);
+        Actor actorRef1 = context.getBean("actor", Actor.class);
+
 //        Actor actorRef2 = context.getBean("actor2", Actor.class);
 //        Actor actorRef3 = context.getBean("actor3", Actor.class);
 
-        System.out.println(actorRef.toString());
+        System.out.println(actorRef1.toString());
+
 //        System.out.println(actorRef2.toString());
 //        System.out.println(actorRef3.toString());
 
 
 
-        Movie movieRef = context.getBean("movie",Movie.class);
+        Movie movieRef1 = context.getBean("movie1",Movie.class);
 
-        System.out.println(movieRef.toString());
+        System.out.println(movieRef1.toString());
+
+        Movie movieRef2 = context.getBean("movie2",Movie.class);
+
+        System.out.println(movieRef2.toString());
 
 
 
