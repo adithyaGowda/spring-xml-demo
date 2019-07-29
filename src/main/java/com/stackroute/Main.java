a1ab2774f3f13aebb3d2e2a5b59065b886f6991b
adithyaGowda <adithyagowda5696@gmail.com>
2019-07-29 06:39:45 +0530

    adding files to aware-interface

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -4,6 +4,7 @@ import com.stackroute.domain.Actor;
 import com.stackroute.domain.Movie;
 
 
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -13,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Actor actorRef1 = context.getBean("actor", Actor.class);
+        Actor actorRef1 = context.getBean("actor1", Actor.class);
         Actor actorRef2 = context.getBean("actor2", Actor.class);
         Actor actorRef3 = context.getBean("actor3", Actor.class);
 
@@ -21,11 +22,14 @@ public class Main {
         System.out.println(actorRef2.toString());
         System.out.println(actorRef3.toString());
 
-
-
-        Movie movieRef1 = context.getBean("movie1",Movie.class);
+        Movie movieRef1 = context.getBean("movieBean1",Movie.class);
+        Movie movieRef2 = context.getBean("movieBean2",Movie.class);
 
         System.out.println(movieRef1.toString());
+        System.out.println(movieRef2.toString());
+
+
+
 
        
 
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,8 +1,17 @@
 package com.stackroute.domain;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements ApplicationContextAware, BeanNameAware, BeanFactoryAware {
 
     Actor actor1, actor2, actor3;
+    ApplicationContext applicationContext;
+    BeanFactory beanFactory;
 
     public void setActor1(Actor actor1) {
         this.actor1 = actor1;
@@ -17,11 +26,36 @@ public class Movie {
     }
 
     @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+        this.applicationContext = applicationContext;
+        System.out.println("testing context");
+
+    }
+
+    @Override
+    public void setBeanName(String s) {
+
+        System.out.println("Bean name is "+s);
+
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+        this.beanFactory = beanFactory;
+        System.out.println("testing beanfactory");
+
+    }
+
+    @Override
     public String toString() {
         return "Movie{" +
                 "actor1=" + actor1 +
                 ", actor2=" + actor2 +
                 ", actor3=" + actor3 +
+                ", applicationContext=" + applicationContext +
+                ", beanFactory=" + beanFactory +
                 '}';
     }
 }
